3aae1ef6c87ea24ec87aa22c3ef04458089c27be
Allan Coutarel <allancoutarel@orange.fr>
2023-10-10 11:55:40 +0200

    Creation of the class "Person"

---
+++ models/person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, id: int, date_of_birth: str, first_name: str, last_name: str):
+        self.id = id
+        self.date_of_birth = date_of_birth
+        self.first_name = first_name
+        self.last_name = last_name
